da49a6b1417d5429f14cb17975f4f8b3c46f841b
Jacob Erholt <56915193+JHErholt@users.noreply.github.com>
2021-08-12 09:07:03 +0200

    Delete inc/inc directory

--- inc/inc/js/prepare.js
+++
@@ -1,9 +0,0 @@
-const getPrepareContainer = document.getElementById('prepareContainer')
-
-const page = document.getElementById('page');
-page.className = 'home'
-
-const colorScheme = document.getElementById('colorScheme')
-const news = document.getElementById('new')
-
-
