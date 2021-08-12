eb5af8850dfbb1a64848bca3d5096b9e449450b6
Jacob Erholt <56915193+JHErholt@users.noreply.github.com>
2021-08-12 09:06:31 +0200

    Add files via upload

---
+++ inc/js/prepare.js
@@ -0,0 +1,9 @@
+const getPrepareContainer = document.getElementById('prepareContainer')
+
+const page = document.getElementById('page');
+page.className = 'home'
+
+const colorScheme = document.getElementById('colorScheme')
+const news = document.getElementById('new')
+
+
